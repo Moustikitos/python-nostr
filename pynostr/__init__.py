1c0c232ab386eb5ba6459f9fddb14a655cca900c
Moustikitos <moustikitos@gmail.com>
2022-12-27 12:08:51 +0100

    docs: docstring update refactor: load_contact used by dump_contact if needeed

--- pynostr/__init__.py
+++ pynostr/__init__.py
@@ -1,16 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-**`pynostr`** aims to provide a simple python interface to interact with nostr
-environment.
-
-```python
->>> from pynostr import event
->>> e = event.Event.text_note("Hello nostr !").sign()
-Type or paste your passphrase >
->>> e.send_to("wss://relay.nostr.info")
-['OK', '2781d70bd1497a8db1ac381262de29841d44fd88d387f2725d5e5eb7871d28c9', Tru\
-e, '']
-```
+This module provides nostr basic functionalities.
 """
 
 import os
@@ -20,12 +10,64 @@ import websockets
 from collections import namedtuple
 from pynostr import bech32
 
-#: Contact is defined by a public key, a relay and a petname
+#: Contact is defined by a public key, a relay and a petname. It is implemented
+#: as a `namedtuple` with `pubkey`, `relay` and `petname` fieldnames.
+#: ```python
+#: >>> import pynostr
+#: >>> contac = Contact(
+#: ...    pubkey=
+#: ...      "9aa650df414cc71b37176412aecba987727b6f1b8cd550f40e9ca10b7af5a239",
+#: ...    relay="wss://relay.nostr.info",
+#: ...    petname="Toon's"
+#: ... )
+#: >>> print(contact)
+#: Contact(pubkey='9aa65[...]5a239', relay='wss:/[...].info', petname="Toon's")
+#: ```
 Contact = namedtuple('Contact', 'pubkey relay petname')
 
 
-class Bech32DecodeError(Exception):
-    """Exception used for unsuccessful bech32 processing"""
+def dump_contact(name: str, *contacts) -> None:
+    """
+Store a list of [contact](#pynostr.Contact) as json format. Files are stored in
+`<pynostr.__path__[0]>/.contact` folder. Folder `.contact` is created if
+needed.
+
+Arguments:
+    name (str): filename to be used.
+    *contact: variable length of [contact](#pynostr.Contact)
+"""
+    # get the file path and create folders if needed
+    filename = os.path.join(os.path.dirname(__path__[0]), ".contact", name)
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    # if file already created, load it to avoid loosing them
+    if os.path.exists(filename):
+        contacts += tuple(
+            [
+                ctc for ctc in load_contact(name)
+                if ctc[0] not in [c[0] for c in contacts]
+            ]
+        )
+    with open(filename, "w") as out:
+        out.write(json.dumps(list(contacts), indent=2))
+
+
+def load_contact(name: str) -> list:
+    """
+Load a list of [contact](#pynostr.Contact) from a file stored in
+`<pynostr.__path__[0]>/.contact` folder. Return an empty list if file or
+`.contact` folder do not exist.
+
+Arguments:
+    name (str): filename to be used.
+Returns:
+    list: list of [contact](#pynostr.Contact).
+"""
+    filename = os.path.join(os.path.dirname(__path__[0]), ".contact", name)
+    if os.path.exists(filename):
+        with open(filename, "r") as _in:
+            contacts = [Contact(*ctc) for ctc in json.loads(_in.read())]
+        return contacts
+    return []
 
 
 async def send_event(event: dict, uri: str) -> list:
@@ -35,7 +77,6 @@ Push single event to a single relay and return responses.
 Args:
     event (dict): the event given as a python dict.
     uri (str): relay uri.
-
 Returns:
     list: relay response as python list.
 """
@@ -47,34 +88,31 @@ Returns:
     return result
 
 
-def dump_contact(name: str, *contacts) -> None:
-    new_pubkey = [c[0] for c in contacts]
-    filename = os.path.join(os.path.dirname(__file__), ".contact", name)
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
-    if os.path.exists(filename):
-        with open(filename, "r") as _in:
-            for ctc in json.loads(_in.read()):
-                if ctc[0] not in new_pubkey:
-                    contacts += (Contact(*ctc), )
-    with open(filename, "w") as out:
-        out.write(json.dumps(list(contacts), indent=2))
+def to_bech32(prefix: str, hexa: str) -> str:
+    """
+Encode string to a prefixed nostr-bech32 string.
 
-
-def load_contact(name: str) -> None:
-    filename = os.path.join(os.path.dirname(__file__), ".contact", name)
-    if os.path.exists(filename):
-        with open(filename, "r") as _in:
-            contacts = [Contact(*ctc) for ctc in json.loads(_in.read())]
-        return contacts
-    return []
-
-
-def to_bech32(prefix: str, hexa: str):
+Args:
+    prefix (str): prefix to be used by bech32 encoder.
+    hexa (str): string to be encoded.
+Returns:
+    str: encoded string.
+"""
     converted_bits = bech32.convertbits(bytes.fromhex(hexa), 8, 5)
     return bech32.bech32_encode(prefix, converted_bits, bech32.Encoding.BECH32)
 
 
-def from_bech32(b32: str):
+def from_bech32(b32: str) -> str:
+    """
+Decode a nostr-bech32 string to hexadecimal string.
+
+Args:
+    b32 (str): nostr-bech32 encoded string.
+Returns:
+    str: decoded string.
+Raises:
+    Bech32DecodeError: if error occurs within bech32 module.
+"""
     data, success = bech32.bech32_decode(b32)[1:]
     if success:
         return bytearray(bech32.convertbits(data, 5, 8))[:-1].hex()
@@ -89,7 +127,6 @@ https://github.com/nostr-protocol/nips/blob/master/19.md)
 
 Args:
     pubkey (str): public key as hex string
-
 Returns:
     str: nostr public key
 """
@@ -103,7 +140,6 @@ https://github.com/nostr-protocol/nips/blob/master/19.md)
 
 Args:
     prvkey (str): private key as hex string
-
 Returns:
     str: nostr private key
 """
@@ -117,8 +153,12 @@ https://github.com/nostr-protocol/nips/blob/master/19.md)
 
 Args:
     noteid (str): event id as hex string
-
 Returns:
     str: nostr event id
 """
     return to_bech32("note", noteid)
+
+
+class Bech32DecodeError(Exception):
+    """Exception used for unsuccessful bech32 processing"""
+    pass
